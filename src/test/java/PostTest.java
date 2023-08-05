d1f8a31bf02c7d533c5f2d4a1bbcdd6987ee36ab
Darya Tarasova <kuroifreya@gmail.com>
2023-08-05 19:39:22 +0300

    initial commit

---
+++ src/test/java/PostTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostTest {
+
+    @Test
+    void shouldPostSomeData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("We're all mad here. I'm mad. You're mad.") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body(/* --> ваша проверка здесь <-- */)
+        ;
+    }
+
+}
